0bab1e3d99b1ac69b50b4624d41b6a247227a122
Valentin Grinkevich <valentin_grinkevich@epam.com>
2020-08-19 08:51:58 +0300

    init

---
+++ src/pages/checker.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+export default function checker() {
+    if(document.cookie.includes('epam-club-app')){
+        let id = document.cookie.split(':')[1]
+        alert('EPAM club user id:' + id)
+      } else {
+        alert('NOT EPAM CLUB USER')
+      }
+  return <> 
+    <Helmet title='asdasd1213'>
+    </Helmet>
+  </>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Helmet from "react-helmet"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  return <> 
+  <Helmet title='asdasd1213'>
+    </Helmet>
+  <div>
+  </div>
+  </>
 }
